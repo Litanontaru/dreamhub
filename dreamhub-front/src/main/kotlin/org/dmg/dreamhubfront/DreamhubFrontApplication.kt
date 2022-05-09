122b9e1e7164169ddbefd7b17263a0d35c58da71
litanontaru <Ghtvmthf5%t>
2022-05-09 17:55:48 +0200

    added module for front

---
+++ dreamhub-front/src/main/kotlin/org/dmg/dreamhubfront/DreamhubFrontApplication.kt
@@ -0,0 +1,11 @@
+package org.dmg.dreamhubfront
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DreamhubFrontApplication
+
+fun main(args: Array<String>) {
+	runApplication<DreamhubFrontApplication>(*args)
+}
